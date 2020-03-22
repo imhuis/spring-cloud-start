72951a6c62fc5e30da4815fa83aaaf3ca49b9c59
zyixh <yjh19980105@gmail.com>
2020-03-22 18:07:52 +0800

    add spring-cloud-config

---
+++ spring-cloud-config-server/src/main/java/com/imhui/cloud/configserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.imhui.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+}
